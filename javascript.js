b5400cb2f0193a8839a1291b7d59710fedc4c0ce
Davide Spigarelli <davide.spigarelli@libero.it>
2024-12-30 14:39:15 +0100

    Aggiunto nuovo file e corrette tutte le pagine

--- javascript.js
+++ javascript.js
@@ -1,9 +1,7 @@
 function facebook() {
-    window.location.href = "https://www.facebook.com/progetto.insieme/";
+    window.location.href = "https://www.facebook.com/";
 }
 function instagram() {
-    window.location.href = "https://www.instagram.com/insiemeprogetto/";
+    window.location.href = "https://www.instagram.com/";
 }
-function maps() {
-    window.location.href = "https://www.google.it/maps/place/Progetto+Insieme+Sigillo/@43.3314989,12.7381159,17z/data=!3m1!4b1!4m6!3m5!1s0x132c357a480413c7:0x953851c52da724c!8m2!3d43.331495!4d12.7406962!16s%2Fg%2F11vppcdz5w?entry=ttu";
-}
+
